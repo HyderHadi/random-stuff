390e72011a130eece804499b3f64fc0f4f29da94
Hyder Hadi <hyder@hyderhadi.xyz>
2026-05-04 18:18:45 +0300

    Finished writing the loop and the Echo function is ready :D

--- simple-C-programs/DynamicString.c
+++ simple-C-programs/DynamicString.c
@@ -14,12 +14,29 @@ int main() {
     Dynamic_string = malloc(string_length);
     
     int input;
+    int ch_count = 0;
     while((input = getchar()) != '\n' && input != EOF) {
-        
+        Dynamic_string[ch_count] = input;
+        ch_count++;
+
+        if(ch_count > (string_length - 5)) {
+            string_length *= 2;
+            char *tmp = realloc(Dynamic_string, string_length);
+            if(!tmp) {
+                printf("Allocation failed!\n");
+                free(Dynamic_string);
+                return 0;
+            }
+
+            Dynamic_string = tmp;
+
+        }
     }
 
 
-
+    Dynamic_string[ch_count] = '\0';
+    printf("%s\n", Dynamic_string);
+    free(Dynamic_string);
 
     return 0;
 }
